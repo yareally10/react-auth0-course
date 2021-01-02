9891b420cfeb70d5b36e9b29417afc3b182302f6
Lin Yi <lin.yi@omnicell.com>
2021-01-02 17:47:45 -0500

    update user profile page

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,6 +3,7 @@ import auth0 from "auth0-js";
 export default class Auth {
   constructor(history) {
     this.history = history;
+    this.userProfile = null;
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -20,7 +21,7 @@ export default class Auth {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
-        this.history.push("/");
+        this.history.push("/profile");
       } else if (err) {
         this.history.push("/");
         alert(`Error: ${err.error}. Checkout console for futher details`);
@@ -49,10 +50,27 @@ export default class Auth {
     localStorage.removeItem("access_token");
     localStorage.removeItem("id_token");
     localStorage.removeItem("expires_at");
+    this.userProfile = null;
     // this.history.push("/"); // soft logout
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       returnTo: "http://localhost:3000",
+    });
+  };
+
+  getAccessToken = () => {
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) {
+      throw new Error("No access token found");
+    }
+    return accessToken;
+  };
+
+  getProfile = (cb) => {
+    if (this.userProfile) return cb(this.userProfile);
+    this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+      if (profile) this.userProfile = profile;
+      cb(profile, err);
     });
   };
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,37 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { Component } from "react";
 
 class Profile extends Component {
+  state = {
+    profile: null,
+    error: "",
+  };
+
+  componentDidMount() {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    this.props.auth.getProfile((profile, error) =>
+      this.setState({ profile, error })
+    );
+  }
+
   render() {
-    return <h1>Profile</h1>;
+    const { profile } = this.state;
+    if (!profile) return null;
+    return (
+      <>
+        <h1>Profile</h1>
+        <p>{profile.nickname}</p>
+        <img
+          style={{ maxWidth: 50, maxHeight: 50 }}
+          src={profile.picture}
+          alt="profile picture"
+        />
+        <pre>{JSON.stringify(profile, null, 2)}</pre>
+      </>
+    );
   }
 }
 
