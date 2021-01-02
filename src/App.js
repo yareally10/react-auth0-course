74dadd69bd4cc5cd7f0ac5226ac5a723353002f6
Lin Yi <lin.yi@omnicell.com>
2021-01-02 17:14:30 -0500

    add logout

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <>
-        <Nav />
+        <Nav auth={this.auth} />
         <div className="body">
           <Route
             path="/"
