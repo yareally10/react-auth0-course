4cdab3dedca9ce4bea7cd0db1e10842687fb291b
Lin Yi <lin.yi@omnicell.com>
2021-01-03 17:02:51 -0500

    use memory to store auth data instead of local storage

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,6 +2,12 @@ import auth0 from "auth0-js";
 
 const REDIRECT_ON_LOGIN = "redirect_on_login";
 
+// Private variables to store auth data
+let _idToken = null; // eslint-disable-line no-unused-vars
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
+
 export default class Auth {
   constructor(history) {
     this.history = history;
@@ -44,32 +50,22 @@ export default class Auth {
 
   setSession = (authResult) => {
     // set the time that access token expires
-    const expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
+    _expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
 
     // If there is a value on the "scope" param from the authResult, use it to
     // set the scopes in the session for the user. Otherwise use the scopes as
     // requested. If no scopes where requested, set it to nothing.
-    const scopes = authResult.scope || this.requestedScopes || "";
+    _scopes = authResult.scope || this.requestedScopes || "";
 
-    localStorage.setItem("access_token", authResult.accessToken);
-    localStorage.setItem("id_token", authResult.idToken);
-    localStorage.setItem("expires_at", expiresAt);
-    localStorage.setItem("scopes", JSON.stringify(scopes));
+    _accessToken = authResult.accessToken;
+    _idToken = authResult.idToken;
   };
 
   isAuthenticated() {
-    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-    return new Date().getTime() < expiresAt;
+    return new Date().getTime() < _expiresAt;
   }
 
   logout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
-    localStorage.removeItem("scopes");
-    this.userProfile = null;
     // this.history.push("/"); // soft logout
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -78,11 +74,10 @@ export default class Auth {
   };
 
   getAccessToken = () => {
-    const accessToken = localStorage.getItem("access_token");
-    if (!accessToken) {
+    if (!_accessToken) {
       throw new Error("No access token found");
     }
-    return accessToken;
+    return _accessToken;
   };
 
   getProfile = (cb) => {
@@ -94,9 +89,7 @@ export default class Auth {
   };
 
   userHasScopes(scopes) {
-    const grantedScopes = (
-      JSON.parse(localStorage.getItem("scopes")) || ""
-    ).split(" ");
+    const grantedScopes = (_scopes || "").split(" ");
     return scopes.every((scope) => grantedScopes.includes(scope));
   }
 }
