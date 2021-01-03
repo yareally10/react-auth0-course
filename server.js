df78d4ecb276a9680a8aa9aab8ad01f49ecd97ca
Lin Yi <lin.yi@omnicell.com>
2021-01-03 12:35:43 -0500

    add a private endpoint and securing it with jwt

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require("express");
 require("dotenv").config();
+const jwt = require("express-jwt"); // Validate JWT and set req.user
+const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from JSON Web Key set (JWKS) endpoint
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the
+  // signing keys provided by the JWKS endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`, // Need the / at the end
+
+  // Must match the algorithm selected in Auth0 dashboard for this api
+  algorithms: ["RS256"],
+});
 
 const app = express();
 
 app.get("/public", function(req, res) {
   res.json({
-    message: "Hello from public API.",
+    message: "Hello from a public API.",
+  });
+});
+
+app.get("/private", checkJwt, function(req, res) {
+  res.json({
+    message: "Hello from a private API!",
   });
 });
 
--- src/Nav.js
+++ src/Nav.js
@@ -16,6 +16,11 @@ class Nav extends Component {
           <li>
             <Link to="/public">Public</Link>
           </li>
+          {isAuthenticated() && (
+            <li>
+              <Link to="/private">Private</Link>
+            </li>
+          )}
           <li>
             <button onClick={isAuthenticated() ? logout : login}>
               {isAuthenticated() ? "Log Out" : "Log In"}
