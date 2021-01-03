e552fef57fa7395e09938cc724e874c3a8771c42
Lin Yi <lin.yi@omnicell.com>
2021-01-03 16:52:49 -0500

    add auth object to react context

--- src/App.js
+++ src/App.js
@@ -9,38 +9,41 @@ import Public from "./Public";
 import Private from "./Private";
 import Courses from "./Courses";
 import PrivateRoute from "./PrivateRoute";
+import AuthContext from "./AuthContext";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.auth = new Auth(this.props.history);
+    this.state = {
+      auth: new Auth(this.props.history),
+    };
   }
 
   render() {
+    const { auth } = this.state;
     return (
-      <>
-        <Nav auth={this.auth} />
+      <AuthContext.Provider value={auth}>
+        <Nav auth={auth} />
         <div className="body">
           <Route
             path="/"
             exact
-            render={(props) => <Home auth={this.auth} {...props} />}
+            render={(props) => <Home auth={auth} {...props} />}
           />
           <Route
             path="/callback"
-            render={(props) => <Callback auth={this.auth} {...props} />}
+            render={(props) => <Callback auth={auth} {...props} />}
           />
-          <PrivateRoute path="/profile" auth={this.auth} component={Profile} />
           <Route path="/public" component={Public} />
-          <PrivateRoute path="/private" auth={this.auth} component={Private} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/private" component={Private} />
           <PrivateRoute
             path="/courses"
-            auth={this.auth}
             component={Courses}
             scopes={["read:courses"]}
           />
         </div>
-      </>
+      </AuthContext.Provider>
     );
   }
 }
